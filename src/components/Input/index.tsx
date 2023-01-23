02d4831bd90dee5ca5b69ba676ccfe910bbada44
Bruna Paes <brunapaesms@gmail.com>
2023-01-23 16:15:01 -0300

    created 'groups' and 'new group' screens

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles';
+
+import { useTheme } from 'styled-components/native';
+
+import { TextInputProps } from 'react-native';
+
+export function Input({...rest}: TextInputProps){
+
+    const { COLORS } = useTheme();
+
+    return(
+       <S.Container 
+        placeholderTextColor={COLORS.GRAY_300}
+        {...rest}
+       /> 
+    )
+}
---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@components/Button';
+
+import { Header } from '@components/Header';
+
+import { Highlight } from '@components/Highlight';
+
+import { Input } from '@components/Input';
+
+import * as S from './styles';
+
+export function NewGroup() {
+    return (
+        <S.Container>
+            <Header showBackButton />
+
+            <S.Content>
+                <S.Icon />
+
+                <Highlight
+                    title='Nova turma'
+                    subtitle='cria a turma para adicionar pessoas'
+                />
+
+                <Input 
+                    placeholder='Nome da turma'
+                />
+
+                <Button
+                    title='Criar'
+                    style={{ marginTop: 20 }}
+                />
+
+            </S.Content>
+        </S.Container>
+    )
+}
